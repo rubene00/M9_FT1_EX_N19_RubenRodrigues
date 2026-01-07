149662f18679c4763c5258537779cc1b3f10ac91
rubene00 <rgrvsc2009@gmail.com>
2026-01-07 12:40:39 +0000

    alterar nome

---
+++ Ex1/M9_FT1_Ex1_rubenM9.py
@@ -0,0 +1,14 @@
+nome = "Ruben"
+numero = 19
+altura = 1.80
+inscrito = 'true'
+
+print(nome)
+print(numero)
+print(altura)
+print(inscrito)
+
+print(type(nome))
+print(type(numero))
+print(type(altura))
+print(type(inscrito))
